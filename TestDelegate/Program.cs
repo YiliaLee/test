fc7d79cf764a8974c0152c019bd14309c82eac51
lingli.li <lilinglizbm@126.com>
2016-08-10 18:35:25 +0800

    添加TestDelegate项目

---
+++ TestDelegate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 委托
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mydelegate md = new Mydelegate(M1);
+            md();
+            Console.WriteLine("OK");
+            Console.ReadKey();
+        }
+        static void M1()
+        {
+            Console.WriteLine("11111");
+        }
+    }
+    public delegate void Mydelegate();
+}
